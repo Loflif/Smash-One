5f660c187be7344548adabe8347d80244af0fb7c
Martin Lövlie <Internecio88@gmail.com>
2020-06-22 15:34:14 +0200

    Added Enemy prefab
    Added Enemy prefab

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = transform.position - player.transform.position;
+        direction.Normalize();
+        direction *= speed;
+
+        transform.position -= direction *Time.deltaTime;
+
+    }
+}
