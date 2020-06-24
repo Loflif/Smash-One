3c384cd195fcb2a2f4b0dfbe53beff1bb1dad5bc
Martin Lövlie <Internecio88@gmail.com>
2020-06-24 15:06:35 +0200

    shooting while holding down button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,10 +18,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform ShotParent;
 
     [SerializeField] private float MovementSpeed = 2.0f;
-   
+
+    [SerializeField] private float ShootCooldown;
+    private float ShootTimer = 0;
+
 
     void Start()
     {
+        ShootTimer = 0;
         if(Shot == null)
         {
             Debug.LogError("Player Shot prefab not linked");
@@ -39,14 +43,19 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(DownKey)) Move(Direction.SOUTH);
         else if (Input.GetKey(LeftKey)) Move(Direction.WEST);
 
-        if (Input.GetKeyDown(UpShootKey) && Input.GetKeyDown(RightShootKey)) Shoot(Direction.NORTH_EAST);
-        else if (Input.GetKeyDown(RightShootKey) && Input.GetKeyDown(DownShootKey)) Shoot(Direction.SOUTH_EAST);
-        else if (Input.GetKeyDown(LeftShootKey) && Input.GetKeyDown(DownShootKey)) Shoot(Direction.SOUTH_WEST);
-        else if (Input.GetKeyDown(LeftShootKey) && Input.GetKeyDown(UpShootKey)) Shoot(Direction.NORTH_WEST);
-        else if (Input.GetKeyDown(UpShootKey)) Shoot(Direction.NORTH);
-        else if (Input.GetKeyDown(RightShootKey)) Shoot(Direction.EAST);
-        else if (Input.GetKeyDown(DownShootKey)) Shoot(Direction.SOUTH);
-        else if (Input.GetKeyDown(LeftShootKey)) Shoot(Direction.WEST);
+        ShootTimer -= Time.deltaTime;
+
+        if(ShootTimer > 0)
+            return;
+
+        if (Input.GetKey(UpShootKey) && Input.GetKey(RightShootKey)) Shoot(Direction.NORTH_EAST);
+        else if (Input.GetKey(RightShootKey) && Input.GetKey(DownShootKey)) Shoot(Direction.SOUTH_EAST);
+        else if (Input.GetKey(LeftShootKey) && Input.GetKey(DownShootKey)) Shoot(Direction.SOUTH_WEST);
+        else if (Input.GetKey(LeftShootKey) && Input.GetKey(UpShootKey)) Shoot(Direction.NORTH_WEST);
+        else if (Input.GetKey(UpShootKey)) Shoot(Direction.NORTH);
+        else if (Input.GetKey(RightShootKey)) Shoot(Direction.EAST);
+        else if (Input.GetKey(DownShootKey)) Shoot(Direction.SOUTH);
+        else if (Input.GetKey(LeftShootKey)) Shoot(Direction.WEST);
     }
 
     private void Move(Direction pDirection)
@@ -86,6 +95,7 @@ public class PlayerController : MonoBehaviour
 
     private void Shoot(Direction pDirection)
     {
+        ShootTimer = ShootCooldown;
         switch (pDirection)
         {
             case Direction.NORTH:
